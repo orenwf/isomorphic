432fa0b498b6a19e77702946c08e2e6d5876a784
orenwf <ofriedman@acm.org>
2017-10-11 18:43:18 -0400

    rewrote isomorphic as anagram to comply with requirements

--- anagram.cpp
+++ anagram.cpp
@@ -5,6 +5,8 @@
 #include <unordered_map>
 #include <utility>
 #include <iostream>
+#include <functional>
+#include <type_traits>
 
 std::unordered_map<std::string, unsigned> make_table(const std::string& s) {
     std::unordered_map<std::string, unsigned> u_map;
@@ -15,9 +17,9 @@ std::unordered_map<std::string, unsigned> make_table(const std::string& s) {
     return u_map;
 }
 
-bool test_isomorphic(const std::string& a, const std::string& b) {
+bool test_anagram(const std::string& a, const std::string& b) {
     if(b.size() != a.size()) return false;
-    std::unordered_map<std::string, unsigned> map_a = make_table(a);
+    auto map_a = make_table(a);
     return map_a == make_table(b);
 }
 
@@ -29,8 +31,8 @@ int main() {
     std::cout << "Please enter two strings: ";
     std::cin >> first >> second;  
 
-    std::string t_or_f = test_isomorphic(first, second) ? "isomorphic " : "not isomorphic ";
-    std::cout << "String " << first << " is " << t_or_f << "with " << second << "." << std::endl;
+    std::string t_or_f = test_anagram(first, second) ? "an anagram " : "not an anagram ";
+    std::cout << "String " << first << " is " << t_or_f << "of " << second << "." << std::endl;
 
     return 0;
 }
